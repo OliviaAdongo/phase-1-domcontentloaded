690b14c7db47033482455613009a4bcdaf902551
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-13 22:56:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+//document.getElementById("text");
+
+function callBack(){
+    document.getElementById("text").textContent = "This is really cool!";
+}
+    document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+        console.log(callBack());
+        
+        });   
+
+
+    
+
+
+
+
+
+
+
+
+
+        // document.getElementById("text").textContent = "This is really cool!";
+
+
+    // document.addEventListener("DOMContentLoaded", function() {
+    //     console.log("The DOM has loaded");
+    //   });
+    //   console.log(
+    //     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+        
